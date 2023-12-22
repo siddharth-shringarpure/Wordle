8ad634ac45daf0a5cc33bd459ac31a9f6dafa75b
sid <35936587+siddharth-shringarpure@users.noreply.github.com>
2023-12-22 20:54:28 +0000

    Refactoring, more modularised code and better file handling.

--- main.py
+++ main.py
@@ -1,81 +1,82 @@
 import colorama
 import random
 
-file = open("5letters.txt", "r")
-wordList = file.readlines()
-numWords = len(wordList)
 
-# Add each item from file to a list of words and remove new line character from each word
-for i in range(0, numWords):
-    wordList[i] = str(wordList[i]).strip("\n")
+def initialise_game():
+    try:
+        with open("5letters.txt", "r") as file:
+            wordList = [word.strip() for word in file]
+    except FileNotFoundError:
+        print("File not found.")
+        exit()
 
+    answer = random.choice(wordList)
+    # print(answer)
+    displayed_word = [""] * 5  # Format each character of input later on
+
+    history = []
+    return wordList, answer, displayed_word, history
+
+
+# Define colours
 GREEN = colorama.Fore.GREEN
 RED = colorama.Fore.MAGENTA
 GREY = colorama.Fore.RESET
 YELLOW = colorama.Fore.YELLOW
 
-answer = random.choice(wordList)
-print(answer)
-formattedInput = [""] * 5  # Format each character of input later on
 
-history = []
+def check_letters(answer, guess_word, displayed_word, history, guesses):
+    if guess_word in history:  # Repeated outputs do not add to number of guesses
+        return False, guesses
 
-def check_letters(guess_word):
-    global guesses, gameEnd, history
-
-    if guess_word in history:  # Checks to see if user has repeated an input
-        return
-    else:
-        history.append(guess_word)  # Stores a list of all entered words for session
+    history.append(guess_word)  # Stores a list of all entered words for session
 
     for j in range(0, len(guess_word)):
 
         if guess_word[j] == answer[j]:
-            # print("yes", str(i), "position")
-            formattedInput[j] = GREEN + guess_word[j]
+            displayed_word[j] = GREEN + guess_word[j]
 
         elif guess_word[j] in answer:
-            # print("letter: ", input[i], "in answer")
-            formattedInput[j] = YELLOW + guess_word[j]
+            displayed_word[j] = YELLOW + guess_word[j]
 
         else:
-            formattedInput[j] = GREY + guess_word[j]
+            displayed_word[j] = GREY + guess_word[j]
 
-    print(*formattedInput)
+    print(*displayed_word)
     # "*" is an unpacking operator; ensures each value from the list is printed on one line
 
     guesses += 1
 
     if guess_word == answer:
-        gameEnd = True
+        return True, guesses
+
+    return False, guesses
 
 
 def get_user_input():
     while True:
         guess = input(GREY + "Enter a five letter word: ")
         if len(guess) != 5:
-            None
+            continue
         else:
             break
     return guess
 
 
-gameEnd = False
-guesses = 0
+if __name__ == "__main__":
+    print(("*" * 16) + "\nUNLIMITED WORDLE\n" + ("*" * 16) + "\n")
 
-print(("*" * 16) + "\nUNLIMITED WORDLE\n" + ("*" * 16) + "\n")
+    wordList, answer, displayed_word, history = initialise_game()
 
-while not gameEnd:
-    wordInput = get_user_input().lower()
-    # gameEnd = letterFinder(wordInput)
+    gameEnd = False
+    guesses = 0
 
-    if wordInput not in wordList:  # Checks to see if word is in dictionary
-        print("Not in dict :/")
-    else:
-        check_letters(wordInput)
+    while not gameEnd:
+        wordInput = get_user_input().lower()
 
-# print(GREY + "Correctly guessed in:", guesses, "goes!")
-print(GREY + f"Correctly guessed in {guesses} goes!") if guesses > 1 else print(
-    GREY + f"Correctly guessed in {guesses} go!")
+        if wordInput not in wordList:  # Checks to see if word is in dictionary
+            print("Invalid word, try again :/")
+        else:
+            gameEnd, guesses = check_letters(answer, wordInput, displayed_word, history, guesses)
 
-file.close()
+    print(f"{GREY}Correctly guessed in {guesses} goes!") if guesses > 1 else print(f"{GREY}Correctly guessed in 1 go!")
